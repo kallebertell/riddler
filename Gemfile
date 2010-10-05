37508b211dbffb5cfd7148fb3100724150b1625c
raimo <raimo@79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6>
2010-10-05 17:59:01 +0000

    Add riddler Rails 3 (main) project
    git-svn-id: http://svn2.xp-dev.com/svn/Memphis/riddler/trunk@70 79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source :gemcutter
+
+gem 'rails', '3.0.0'
+gem 'haml'
+git 'http://github.com/arsduo/koala.git' do 
+  gem 'koala'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trunk::Application.config.secret_token = '151cd88cb140a93393bf5470d60c044a3868612c492a4b394c50a598708ea0f32816a5ad6e8e26011226f4c98b7ec219807a0458ec04b6efc32cb7a1f4879f2f'
