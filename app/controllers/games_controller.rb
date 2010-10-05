a6d98cae236f35f954752f0465cbac7f01bb4108
raimo <raimo@79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6>
2010-10-05 20:27:03 +0000

    games#new skeleton
    git-svn-id: http://svn2.xp-dev.com/svn/Memphis/riddler/trunk@83 79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
   def new
-    render :text => "You are logged in as #{current_user.nil? ? 'no one' : current_user.name}."
+    @game = Game.new
   end
 end
