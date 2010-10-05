1bacdef6ac7532c2fc2c3492abd79c973e0bc8c3
raimo <raimo@79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6>
2010-10-05 18:24:25 +0000

    Remove deprecated lines from the way of Rails 3
    git-svn-id: http://svn2.xp-dev.com/svn/Memphis/riddler/trunk@76 79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  include Koala
+  include Authentication
   protect_from_forgery
+
+  before_filter :set_session_variables
+
+  private
+
+  def set_session_variables
+   @oauth = (session[:oauth] ||= Koala::Facebook::OAuth.new(FB_APP_ID, FB_SECRET, URI.escape(create_session_url)))
+   @graph = session[:graph]
+  end
 end
