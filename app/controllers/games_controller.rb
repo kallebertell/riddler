3258cc17da831a8b5e7411a46180a596cc43c8b7
kallebertell <kallebertell@79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6>
2010-10-20 17:36:24 +0000

    old rest api taken into use w/ fql demo. added extended permissions
    git-svn-id: http://svn2.xp-dev.com/svn/Memphis/riddler/trunk@93 79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,36 @@
 class GamesController < ApplicationController
+
   def new
     @game = Game.new
 
-    @query = "SELECT uid, status_id, message FROM status WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = #{@current_user.facebook_id.to_s})";
+    query = "SELECT uid, status_id, message FROM status WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = #{@current_user.facebook_id.to_s})";
 
-    @friends_status = @restapi.fql_query(@query);
+    friends_statuses = @restapi.fql_query(query).sort_by {rand};
 
+    status_to_guess = friends_statuses.at(0)
+    @correct_uid = status_to_guess['uid'];
+    unrelated_uids = get_two_unrelated_uids(friends_statuses, @correct_uid)
+    @uids = unrelated_uids.concat([@correct_uid])
+    @status_message = status_to_guess['message']
+
+    @friends = @restapi.fql_query("SELECT name FROM user WHERE uid IN (#{@uids.join(', ')})").collect {|x| x['name'] }
+  end
+
+  
+  private
+
+  def get_two_unrelated_uids(statuses, correct_uid)
+    unrelated_uids = []
+    cnt = 1;
+
+    while unrelated_uids.size < 2
+      uid = statuses.at(cnt)['uid']
+      unrelated_uids.push(uid) #if (uid != correct_uid)
+      cnt += 1;
+      break if (cnt > 999)
+    end
+
+    unrelated_uids
   end
 
 end
