125df58579af0044f0c29c1872d2bc197d30a71f
raimo <raimo@79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6>
2010-12-07 23:48:44 +0000

    Wrote tests for the enum column
    git-svn-id: http://svn2.xp-dev.com/svn/Memphis/riddler/trunk@161 79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6

--- test/unit/question_test.rb
+++ test/unit/question_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
 
-  test "should create question with type" do
+  test "should create one of the enum types as question type" do
     @game = Factory(:new_game)
     status = Factory(:status, :game_id => @game.id)
     status = Factory(:friend, :game_id => @game.id)
@@ -10,4 +10,27 @@ class QuestionTest < ActiveSupport::TestCase
     assert question.enums(:question_type).include?(question.question_type)
   end
 
+  test "should create status as question type" do
+    @game = Factory(:new_game)
+    status = Factory(:status, :game_id => @game.id)
+    question = @game.questions.create(:question_type => :status)
+    assert question.question_type_status?
+  end
+
+
+  test "should create birthdate as question type" do
+    @game = Factory(:new_game)
+    status = Factory(:friend, :game_id => @game.id)
+    question = @game.questions.create(:question_type => :birthdate)
+    assert question.question_type_birthdate?
+  end
+
+  test "should fail creating bogus question type" do
+    @game = Factory(:new_game)
+    assert_no_difference 'Question.count' do
+      assert_raise EnumeratedAttribute::InvalidEnumeration do
+        question = @game.questions.create(:question_type => :bogus)
+      end
+    end
+  end
 end
