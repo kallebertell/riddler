3f28f9d612389b0a53056a78fb36334052c083b9
raimo <raimo@79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6>
2010-12-08 18:26:02 +0000

    wrote tests for creating or finding existing user on login
    git-svn-id: http://svn2.xp-dev.com/svn/Memphis/riddler/trunk@162 79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -6,4 +6,35 @@ class SessionsControllerTest < ActionController::TestCase
     get :create
     assert_redirected_to '/'
   end
+
+  test "should destroy session" do
+    session[:user_id] = 'foo'
+    get :destroy
+    assert_redirected_to root_path
+    assert_nil session[:user_id]
+  end
+
+  test "should log existing user in" do
+    session[:fb_session] = fb_session = mock()
+    fb_session.expects(:connect).with('testcode')
+    fb_session.stubs(:get_current_user).then.returns({ 'id' => 'testid' })
+    Factory(:user, :facebook_id => 'testid')
+    assert_no_difference('User.count') do
+      post :create, :code => 'testcode'
+      assert_redirected_to root_path
+    end
+    assert_not_nil assigns(:user)
+  end
+
+  test "should create new user when logging in" do
+    session[:fb_session] = fb_session = mock()
+    fb_session.expects(:connect).with('testcode')
+    fb_session.stubs(:get_current_user).then.returns({ 'id' => 'testid' })
+    assert_difference('User.count', 1) do
+      post :create, :code => 'testcode'
+      assert_redirected_to root_path
+    end
+
+    assert_equal assigns(:user), User.find_by_facebook_id('testid')
+  end
 end
