ca161953cc47336302e60807ac7626230b4eb705
raimo <raimo@79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6>
2010-12-20 20:07:22 +0000

    Wrote test for the Canvas page login
    git-svn-id: http://svn2.xp-dev.com/svn/Memphis/riddler/trunk@207 79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -14,7 +14,19 @@ class SessionsControllerTest < ActionController::TestCase
     assert_nil session[:user_id]
   end
 
-  test "should log existing user in" do
+  test "should log existing user in through canvas page" do
+    session[:fb_session] = fb_session = mock()
+    fb_session.stubs(:parse_signed_request).with('signedreq').then.returns({'oauth_token'=>'testtoken'})
+    fb_session.expects(:connect_with_oauth_token).with('testtoken')
+    fb_session.stubs(:get_current_user).then.returns({ 'id' => 'testid' })
+    Factory(:user, :facebook_id => 'testid')
+    assert_no_difference('User.count') do
+      post :login, :signed_request => 'signedreq'
+      assert_redirected_to root_path
+    end
+  end
+
+  test "should log existing user in through the normal login procedure" do
     session[:fb_session] = fb_session = mock()
     fb_session.expects(:connect_with_code).with('testcode')
     fb_session.stubs(:get_current_user).then.returns({ 'id' => 'testid' })
