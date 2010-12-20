7ca15667d0b89ffe79c1244630e82cb1238016d6
raimo <raimo@79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6>
2010-12-20 20:26:28 +0000

    update schema version.
    git-svn-id: http://svn2.xp-dev.com/svn/Memphis/riddler/trunk@210 79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101219144153) do
+ActiveRecord::Schema.define(:version => 20101214232011) do
 
   create_table "choices", :force => true do |t|
     t.integer  "question_id"
