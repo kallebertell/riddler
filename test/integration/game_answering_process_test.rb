b260346960678c2dec24f177ae5bc40df55b64ad
raimo <raimo@79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6>
2010-12-21 23:14:28 +0000

    Wrote integration test which plays two games in a row
    git-svn-id: http://svn2.xp-dev.com/svn/Memphis/riddler/trunk@228 79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6

--- test/integration/game_answering_process_test.rb
+++ test/integration/game_answering_process_test.rb
@@ -5,20 +5,31 @@ class GameAnsweringProcessTest < ActionDispatch::IntegrationTest
 
   test "game answering and getting results" do
     log_in do |user|
-      user.should_get_index
-      user.should_get_redirected_to_first_question_when_game_is_created
-      
-      9.times do 
-        user.should_answer_question_and_get_redirected_to_next_question
-      end
-      
-      user.should_answer_question
-      
-      user.should_see_result
+      play_a_game_as(user)
+    end
+  end
+
+  test "play a game two times" do
+    log_in do |user|
+      play_a_game_as(user)
+      play_a_game_as(user)
     end
   end
 
   private
+
+  def play_a_game_as(user)
+    user.should_get_index
+    user.should_get_redirected_to_first_question_when_game_is_created
+    
+    9.times do 
+      user.should_answer_question_and_get_redirected_to_next_question
+    end
+    
+    user.should_answer_question
+    
+    user.should_see_result
+  end
 
   def log_in
     login_as(Factory(:user)) do
@@ -44,7 +55,9 @@ class GameAnsweringProcessTest < ActionDispatch::IntegrationTest
     end
    
     def should_answer_question
-      choice_links = css_select("#choices a")     
+      choice_links = css_select("#choices a.text")     
+      p choice_links.map(&:to_s)
+      assert_operator choice_links.size, :<, 5
       post choice_links[0]['href']      
       assert_response :redirect
       follow_redirect!
