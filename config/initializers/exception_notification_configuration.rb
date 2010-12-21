6c9bbb990ca8599b032061178fbc4e0c14d79969
raimo <raimo@79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6>
2010-12-21 16:56:26 +0000

    Use notifier in development only.
    git-svn-id: http://svn2.xp-dev.com/svn/Memphis/riddler/trunk@227 79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6

--- config/initializers/exception_notification_configuration.rb
+++ config/initializers/exception_notification_configuration.rb
@@ -1,4 +1,4 @@
-Riddler::Application.config.middleware.use ExceptionNotifier,
+Riddler::Application.config.middleware.use(ExceptionNotifier,
   :email_prefix => "[Riddler@#{Rails.env}] ",
   :sender_address => %{"notifier" <riddler.application@gmail.com>},
-  :exception_recipients => %w{riddler.application@gmail.com}
+  :exception_recipients => %w{riddler.application@gmail.com}) unless Rails.env=='development'
