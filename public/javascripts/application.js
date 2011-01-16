1aa787587f98917913bce180adcd55165711d4c3
raimo <raimo@79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6>
2011-01-16 18:00:19 +0000

    Added loading window for new game creation
    git-svn-id: http://svn2.xp-dev.com/svn/Memphis/riddler/trunk@260 79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,21 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+  $('.create_game').loadingWindow();
+});
+
+
+
+(function($) {
+  $.fn.loadingWindow = function(options) {
+    var options = jQuery.extend({}, options);
+    return this.each(function() {
+      var link = this;
+      $(link).click(function () {
+        $('#container').html('Prepare for the game to begin!');
+      });
+    });
+  };
+})(jQuery);
+
