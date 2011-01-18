dddc147a218b470223ab29fd4ed6121d824c78ac
beerdroid <beerdroid@79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6>
2011-01-18 19:04:45 +0000

    schema merged
    git-svn-id: http://svn2.xp-dev.com/svn/Memphis/riddler/trunk@273 79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20110118173855) do
 
   create_table "choices", :force => true do |t|
