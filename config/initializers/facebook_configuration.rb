03ac4b6fe3a55212a03b167bc1dcb5df6c214ec9
raimo <raimo@79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6>
2011-01-30 02:37:36 +0000

    Change riddler to fiddler in facebook app
    git-svn-id: http://svn2.xp-dev.com/svn/Memphis/riddler/trunk@297 79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6

--- config/initializers/facebook_configuration.rb
+++ config/initializers/facebook_configuration.rb
@@ -2,7 +2,7 @@ if Rails.env == 'production'
   FB_APP_ID = '150102758358340'
   FB_API_KEY = 'cc1cff344718fa271871ccc62cef7dc5'
   FB_SECRET = 'a12e1de81a7917a5f670ae48c282fb98'
-  FB_APP_ROOT = 'http://apps.facebook.com/riddler-/'
+  FB_APP_ROOT = 'http://apps.facebook.com/friddler/'
 else
   FB_APP_ID = '166590666700460'
   FB_API_KEY = '0b7b13df63e3a6daccb0300e091e249d'
