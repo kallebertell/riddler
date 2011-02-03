94c1f69e643e2eca620e92f865afc9f530afd7bc
beerdroid <beerdroid@79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6>
2011-02-03 20:18:42 +0000

    fixed migration to migrate data too
    git-svn-id: http://svn2.xp-dev.com/svn/Memphis/riddler/trunk@310 79bdcdcd-0c1a-450d-b11d-8e0b0b5144a6

--- db/migrate/20110203182334_renew_score_columns.rb
+++ db/migrate/20110203182334_renew_score_columns.rb
@@ -3,6 +3,12 @@ class RenewScoreColumns < ActiveRecord::Migration
      add_column :users, :week_best_score, :integer, :default => 0
      add_column :users, :week_total_score, :integer, :default => 0
      rename_column :users, :alltime_score, :total_score
+     
+     User.find(:all).each do |u|
+        u.week_best_score = 0
+        u.week_total_score = 0
+        u.save
+     end
    end
 
    def self.down
